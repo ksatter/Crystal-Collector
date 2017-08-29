96d06f8109130a57718c107deffc81c9d7f9104c
ksatter <kberis@gmail.com>
2017-08-29 18:09:04 -0400

    "game working"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,111 @@
 //when document loaded
+$(document).ready(function() {
+    // Set global Variables
+    var wins = 0;
+    var losses = 0;
+    var targetScore = 0;
+    var userScore = 0;
+    var crystalValues = [];
+    var crystalImages = [];
+    //Random number generator for target score and crustal values
+    function randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
 
-//set variables
+    //Start new game
+    newGame();
 
-var userScore = 0
-var targetScore = random number
-var
+    //Create target score and update text
+    function newGame() {
+        userScore = 0;
+        // Create target score between 50 and 75nj
+        targetScore = randomNumber(50, 75);
+        $("#current-score").text(userScore);
+        $("#target-score").text(targetScore);
+        $("#player-wins").text(wins);
+        $("#player-losses").text(losses);
+
+        createCrystals();
+
+        $(".gem").click(function () {
+            var pointIncrease = parseInt($(this).attr("value"));
+            userScore = userScore + pointIncrease;
+            $("#current-score").text(userScore);
+            //win
+            if (userScore === targetScore) {
+                wins++;
+
+                $("#last-target").text(targetScore);
+                $("#last-result").text('won');
+                $('#last-score').text(userScore);
+                $('#previous-results').show();
+
+                newGame();
+            }
+            //lose
+            else if (userScore > targetScore) {
+                losses++;
+
+                $("#last-target").text(targetScore);
+                $("#last-result").text('lost');
+                $('#last-score').text(userScore);
+                $('#previous-results').show();
+
+                newGame();
+
+            }
+        });
 
 
-//trigger start
+    }
+    //Create crystals and assigns each a value
+    function createCrystals() {
+        $(".crystals").html('');
+        crystalValues = [];
+        crystalImages = ['assets/images/diamond.png', "assets/images/ruby.png", "assets/images/sapphire.png", "assets/images/topaz.png"];
 
-//when user clicks
+        for (var x = 1; x <= crystalImages.length;) {
+            //create random value between 5 and 15
+            var newNumber = randomNumber(5, 15);
+            //verify that number is not already assigned and push to array until there is a number for every crystal
+            if (crystalValues.indexOf(newNumber) < 0) {
+                crystalValues.push(newNumber);
+                x++;
+            }
+        }
+        //create crystals using images and values, assign appropriate classes for styling
+        for (var i = 0; i < crystalValues.length; i++) {
+            var newCrystal = $('<img>');
+            newCrystal.attr('value', crystalValues[i]);
+            newCrystal.attr('src', crystalImages[i]);
+            newCrystal.attr('alt', 'crystal;');
+            newCrystal.addClass('gem');
+            //insert into html
+            $('.crystals').append(newCrystal);
+        }
+    }
+    //add value of crystal to score, determine game result and update accordingly
 
-//identi
+
+
+    //clear wins and losses and start over
+    function reset() {
+        wins = 0;
+        losses = 0;
+
+        newGame();
+
+    }
+    //trigger reset with button
+    $("#reset").click(function(){
+        reset()
+    });
+
+
+
+
+    console.log(userScore);
+
+})
+
+
